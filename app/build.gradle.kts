2466bc4ecf77d4ae1be2c3e9c5155930cac63206
Hei Lam (Jarvis) <102021121+JarvisLam-LemonCEO@users.noreply.github.com>
2026-08-30 04:34:28 -0700

    code updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.texttospeechapp"
         minSdk = 24
         targetSdk = 37
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 5
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
